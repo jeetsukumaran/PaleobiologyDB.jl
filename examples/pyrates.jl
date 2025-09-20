6005b82d5be16ab04920a620e23da3bdb277ad5c
Jeet Sukumaran <jeetsukumaran@gmail.com>
2025-09-20 06:22:07 -0700

    Add examples

---
+++ examples/pyrates.jl
@@ -0,0 +1,14 @@
+
+
+#=
+
+```bash
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ python3 -m ensurepip --upgrade
+$ python3 -m pip install --upgrade pip
+$ python -m pip install -r https://raw.githubusercontent.com/dsilvestro/PyRate/master/requirements.txt
+```
+
+
+=#
