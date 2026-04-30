4ae283032f05efce572cf6e39e17ecbea830f77c
Jeet Sukumaran <jeetsukumaran@gmail.com>
2026-04-29 23:05:54 -0700

    Update example

--- examples/src/taxonomytree.jl
+++ examples/src/taxonomytree.jl
@@ -1,9 +1,19 @@
+# Set up data caching
 using PaleobiologyDB.DataCaches: set_autocaching!
-using PaleobiologyDB.TaxonomyMakie: taxonomytreeplot 
-using CairoMakie
 
+# A Makie back-end, here "CairoMakie" needs to be 
+# loaded in the environment for `PaleobiologyDB.TaxonomyMakie`
+# to be available
+using CairoMakie 
+
+# Load the taxonomy visualizier
+using PaleobiologyDB.TaxonomyMakie: taxonomytreeplot 
+
+# Save/re-use local copies of query results 
+# instead of re-running repeated queries live
 set_autocaching!(true)
 
+# Visualize the taxonomy
 fig, ax, plt = taxonomytreeplot(
     "Ursidae"; 
     leaf_rank = "species",
